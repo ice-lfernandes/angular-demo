00351458130f1e2dbe8749643d45d0930a406a57
ice_lfernandes <ice_lfernandes@uolinc.com>
2022-04-04 20:20:30 -0300

    example simple component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { WarnningAlertComponent } from './warnning-alert/warnning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarnningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
